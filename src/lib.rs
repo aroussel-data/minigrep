67405a967eca3e705dbba3886920a9a98ea83ef4
aroussel-data <alexandredrr14@gmail.com>
2024-10-05 19:39:00 +0200

    change config build to use iterator

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, filename, ignore_case })
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|_err| {
+    let config = Config::build(env::args()).unwrap_or_else(|_err| {
         eprintln!("Problem parsing arguments: {_err}");
         process::exit(1);
     });
